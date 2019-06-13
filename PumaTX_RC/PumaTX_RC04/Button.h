342bd673db6d10089fc639cee60c3700764511fb
PumaFPV hp <49276509+PumaFPV@users.noreply.github.com>
2019-06-13 10:15:29 +0200

    Started Soft power code in button.h

--- PumaTX_RC/PumaTX_RC04/Button.h
+++ PumaTX_RC/PumaTX_RC04/Button.h
@@ -1,86 +1,27 @@
-//=================================================
-//  MULTI-CLICK:  One Button, Multiple Events
+void SoftPower(){
 
-// Button timing variables
-int debounce = 20;          // ms debounce period to prevent flickering when pressing or releasing the button
-int DCgap = 250;            // max ms between clicks for a double click event
-int holdTime = 1000;        // ms hold period: how long to wait for press+hold event
-int longHoldTime = 3000;    // ms long hold period: how long to wait for press+hold event
+  if(Pwr.Pin == 0){
+    Step = 1;
 
-// Button variables
-boolean buttonVal = HIGH;   // value read from button
-boolean buttonLast = HIGH;  // buffered value of the button's previous state
-boolean DCwaiting = false;  // whether we're waiting for a double click (down)
-boolean DConUp = false;     // whether to register a double click on next release, or whether to wait and click
-boolean singleOK = true;    // whether it's OK to do a single click
-long downTime = -1;         // time the button was pressed down
-long upTime = -1;           // time the button was released
-boolean ignoreUp = false;   // whether to ignore the button release because the click+hold was triggered
-boolean waitForUp = false;        // when held, whether to wait for the up event
-boolean holdEventPast = false;    // whether or not the hold event happened already
-boolean longHoldEventPast = false;// whether or not the long hold event happened already
+    /*
+    Pwr.Time = millis();
+    if(millis() - Pwr.Time < 1200 && millis() - Pwr.Time > 800){
+      Step = 1
+    }
+    */
+  }
 
-int checkButton() {    
-   int event = 0;
-   buttonVal = digitalRead(buttonPin);
-   // Button pressed down
-   if (buttonVal == LOW && buttonLast == HIGH && (millis() - upTime) > debounce)
-   {
-       downTime = millis();
-       ignoreUp = false;
-       waitForUp = false;
-       singleOK = true;
-       holdEventPast = false;
-       longHoldEventPast = false;
-       if ((millis()-upTime) < DCgap && DConUp == false && DCwaiting == true)  DConUp = true;
-       else  DConUp = false;
-       DCwaiting = false;
-   }
-   // Button released
-   else if (buttonVal == HIGH && buttonLast == LOW && (millis() - downTime) > debounce)
-   {        
-       if (not ignoreUp)
-       {
-           upTime = millis();
-           if (DConUp == false) DCwaiting = true;
-           else
-           {
-               event = 2;
-               DConUp = false;
-               DCwaiting = false;
-               singleOK = false;
-           }
-       }
-   }
-   // Test for normal click event: DCgap expired
-   if ( buttonVal == HIGH && (millis()-upTime) >= DCgap && DCwaiting == true && DConUp == false && singleOK == true && event != 2)
-   {
-       event = 1;
-       DCwaiting = false;
-   }
-   // Test for hold
-   if (buttonVal == LOW && (millis() - downTime) >= holdTime) {
-       // Trigger "normal" hold
-       if (not holdEventPast)
-       {
-           event = 3;
-           waitForUp = true;
-           ignoreUp = true;
-           DConUp = false;
-           DCwaiting = false;
-           //downTime = millis();
-           holdEventPast = true;
-       }
-       // Trigger "long" hold
-       if ((millis() - downTime) >= longHoldTime)
-       {
-           if (not longHoldEventPast)
-           {
-               event = 4;
-               longHoldEventPast = true;
-           }
-       }
-   }
-   buttonLast = buttonVal;
-   return event;
+  if(Step == 1 && Pwr.Pin == 1){
+      Pwr.Time = millis();
+      if(millis() - Pwr.Time < 900 && millis() - Pwr.Time > 1100){
+        Step = 2;
+      }
+  }
+
+  if(Step == 2 && Pwr.Pin == 0){
+    Pwr.Time = millis();
+    if(millis() - Pwr.Time < 1800 && millis() - Pwr.Time >  2200)
+    Step = 3;
+  }
+  
 }
