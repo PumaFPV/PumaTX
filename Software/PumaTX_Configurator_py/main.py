7e4968a2e1f65bdf174eba3139f60752c9d3d077
Puma_WS <49276509+PumaFPV@users.noreply.github.com>
2022-09-27 00:51:55 +0200

    GUI working (lol no)

--- Software/PumaTX_Configurator_py/main.py
+++ Software/PumaTX_Configurator_py/main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+from tkinter import *
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+root = Tk()
+root.title('PumaTX Configurator')
+root.iconbitmap('C:/Users/Nico/Pictures/Puma/puma_black.ico')
+img = PhotoImage(file="C:/Users/Nico/Desktop/pumatx/Software/PumaTX_Configurator_py/img/display_full.png")
+root.geometry("720x640")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+display = Canvas(root, width=650+10, height=340+10, bg='#282c34')
+display.pack(pady=20)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+my_image = display.create_image(833, 442, anchor=NW, image=img)
+
+display.create_line(5, 5, 655, 5, fill="white")
+display.create_line(655, 5, 655, 205, fill="white")
+display.create_line(655, 205, 490, 345, fill="white")
+display.create_line(490, 345, 165, 345, fill="white")
+display.create_line(165, 345, 5, 205, fill="white")
+display.create_line(5, 205, 5, 5, fill="white")
+display.create_polygon()
+
+root['background']='#3c3f41'
+root.mainloop()
